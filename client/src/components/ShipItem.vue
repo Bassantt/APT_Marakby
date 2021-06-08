25b6f81b074679569d16ac797372a13ddae9df2f
bassantt <bassantmohamed945@yahoo.com>
2021-06-08 05:31:33 +0200

    manager

---
+++ client/src/components/ShipItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="Ship-item" id="con">
+    <tr>
+      <td><img src="../assets/ship2.jpg" /></td>
+      <td></td>
+      <td>
+        <h3>Name: {{ Ship_Name }}</h3>
+      </td>
+      <td>
+        <h3>Price: {{ Ship_Price }}</h3>
+      </td>
+      <td></td>
+      <td></td>
+      <td><button @click="DeleteShip" class="Delete">X</button></td>
+    </tr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShipItem",
+ props: {
+    Ship_Id: {
+      type: String,
+    },
+    Ship_Name: {
+      type: String,
+    },
+     Ship_Price: {
+      type: String,
+    },
+
+  },
+  methods: {
+    DeleteShip() {
+      console.log("I will delete");
+
+      //will put dispatch here and send the ship id
+    },
+  },
+};
+</script>
+
+<style scoped>
+#con {
+  background: rgb(37, 91, 122);
+  padding: 10px;
+  word-spacing: 5px;
+  border-spacing: 20px;
+  background-color: rgb(96, 155, 189);
+  margin-bottom: 20px;
+  border: solid black;
+  width: 50%;
+}
+
+.Delete {
+  background: #af0909;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
---
+++ client/src/views/Manager.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="cont">
+    <img src="../assets/logo.png" />
+    <button class="btn" @click="LOgoutmanger">Log out</button>
+    <div>
+      <div class="home row col-12 justify-content-center px-0 m-0">
+        <AddShip />
+      </div>
+    </div>
+    <div class="row">
+        <h1> YOUR SHIPS </h1>
+      <ShipItem
+        class="col-lg-100% col-md-60% col-xs-6"
+        v-for=" Ship in ships"
+        :key="Ship.id"
+        :Ship_Id="Ship.id"
+        :Ship_Name="Ship.Name"
+        :Ship_Price="Ship.Price" 
+        :ship_img="Ship.ImageSrc"
+        />
+    </div>    
+  </div>
+</template>
+
+<script>
+import AddShip from "@/components/AddShip.vue";
+import ShipItem from "@/components/ShipItem.vue";
+export default {
+  name: "Manager",
+  components: {
+    AddShip,
+    ShipItem,
+  },
+  data() {
+    return {
+      ships: [
+        {
+          id: "1",
+          Name: "Normandy",
+          Loc: "mazarita",
+          ImageSrc: "../assets/ship4.jpg",
+          Price: "1500",
+          Description: "This is a very good ship",
+          Rate: "5.0",
+          Available: true,
+        },
+
+        {
+          id: "2",
+          Name: "amirelb7ar",
+          Loc: "kotmoto",
+          ImageSrc: "../assets/ship5.jpg",
+          Price: "500",
+          Description: "This is a very bad ship",
+          Rate: "5.0",
+          Available: true,
+        },
+         {
+          id: "1",
+          Name: "Normandy",
+          Loc: "mazarita",
+          ImageSrc: "../assets/ship4.jpg",
+          Price: "1500",
+          Description: "This is a very good ship",
+          Rate: "5.0",
+          Available: true,
+        },
+
+        {
+          id: "2",
+          Name: "amirelb7ar",
+          Loc: "kotmoto",
+          ImageSrc: "../assets/ship5.jpg",
+          Price: "500",
+          Description: "This is a very bad ship",
+          Rate: "5.0",
+          Available: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    LOgoutmanger() {
+      console.log("logOUT");
+      //will put dispatch here
+    },
+  },
+};
+</script>
+
+<style scoped>
+.row
+{
+    margin-top:800px;
+}
+.cont {
+  height: calc(200vh);
+  background-color: rgba(158, 185, 185, 0.596);
+}
+.home {
+  background-repeat: no-repeat;
+  height: calc(70vh);
+  width: 100%;
+  background-position: center;
+  background-size: 100% 100%;
+  position: absolute;
+}
+.btn {
+  background-color: rgb(96, 155, 189);
+  width: 15%;
+  display: inline-block;
+  height: 60px;
+  border-radius: 20px;
+  margin: 50px 0px;
+  color: rgb(37, 91, 122);
+  border: solid black;
+}
+.btn:hover {
+  background-color: rgb(37, 91, 122);
+}
+</style>
