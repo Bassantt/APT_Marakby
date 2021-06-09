f999ffa52107aaab4aa00f3f76d9725f1f899c35
bassantt <bassantmohamed945@yahoo.com>
2021-06-09 19:44:50 +0200

    admin

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -1,19 +1,85 @@
 <template>
   <div class="cont">
+    <HomeNavigation/>
     <div class="row col-12 justify-content-center px-0 m-0"></div>
-    hii I am Admin
-  </div>
+    <h1>I Am The Admin</h1>
+      <div class="user-profile-data">
+        <h3>Name: {{user.userName}}</h3>
+        <h3>Email: {{user.email}}</h3>
+        <h3>Country: {{user.country}}</h3>
+    </div>
+</div>
+
 </template>
 
 <style scoped>
-.cont {
-  height: calc(200vh);
-  background-color: rgb(37, 91, 122);
+.user-profile-data{
+    margin-top: 2%;
+    border: 10px solid rgba(255, 255, 255, 0.877);
+    background: #ffffff;
+    
+    display: inline-block;
+    padding:20px;
+    text-align: justify;
+    margin-left: auto;
+    margin-right: auto;
+}
+.res-item{
+    padding: 0;
+    text-align: center;
+    margin-block: 10px;
+}
+
+.ProfilePic{
+    float: left;
+    margin-left: 20%;
+    margin-top: 5%;
+    max-width: 300px;
+    max-height: 300px;
+    vertical-align: top;    
+    border-radius: 20%;
+}
+
+h3{
+     border-bottom: 2px solid rgb(158, 156, 156);
+}
+
+button{
+display: inline-block;
+    margin: 5%;
+    font-size: 20px;
+}
+
+input[type=text], input[type=password]{
+  width: 30%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  resize: vertical;
+}
+
+.All{
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 3px;
+max-width: 70%;
+    border: 2px solid rgb(221, 221, 221);
 }
 </style>
 
 <script>
+import { mapGetters } from "vuex";
+import HomeNavigation from "@/components/HomeNavigationBar.vue";
 export default {
   name: "Admin",
+  components: {
+    HomeNavigation,
+  },
+  computed: {
+    ...mapGetters({
+      user: "Authorization/user"
+    }),
+  },
 };
 </script>
